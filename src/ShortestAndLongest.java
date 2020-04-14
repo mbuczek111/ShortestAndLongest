39f57e1c0bd36ae0c9bfbff36e8331b428d8d07d
mikolaj <mbuczek111@gmail.com>
2020-04-14 20:32:47 +0200

    Improved homework assignment

--- src/ShortestAndLongest.java
+++ src/ShortestAndLongest.java
@@ -1,27 +1,54 @@
 public class ShortestAndLongest {
 
     public static void main(String[] args) {
-        String str= "For given text find the longest and the shortest words";
+        String str= "For given text find the longest and the shortest words tralalal";
         String [] words= str.split(" ");
-        String sw,lw;
-        sw=words[0];
-        lw=words[0];
+        String [] sw = new String [words.length];
+        String [] lw = new String [words.length];
+
+        sw[0]=words[0];
+        lw[0]=words[0];
+        int scounter=1;
+        int lcounter=1;
         int min=words[0].length();
         int max=words[0].length();
         for(int i=0;i<words.length;i++)
         {
-        if(words[i].length()<min)
+        if(words[i].length()<=min)
             {
             min=words[i].length();
-            sw=words[i];
+            sw[0]=words[i];
             }
-        if(words[i].length()>max)
+        if(words[i].length()>=max)
             {
             max=words[i].length();
-            lw=words[i];
+            lw[0]=words[i];
             }
         }
-        System.out.println("The shortest word: " + sw);
-        System.out.println("The longest words: " + lw);
+        for(int i=0;i<words.length;i++)
+        {
+            if(words[i].length()==min)
+            {
+                sw[scounter-1] = words[i];
+                scounter++;
+            }
+            if(words[i].length()==max)
+            {
+                lw[lcounter-1] = words[i];
+                lcounter++;
+            }
+        }
+
+        System.out.println("The shortest words: ");
+        for(int i=0;i<scounter-1;i++)
+        {
+            System.out.println(sw[i]);
+        }
+        System.out.println("The longest words: ");
+        for(int i=0;i<lcounter-1;i++)
+        {
+            System.out.println(lw[i]);
+        }
+
     }
 }
